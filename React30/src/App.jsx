e7e5603d4ebbc3fec971d429d6ee69b7a115d148
yogesh bansal <bansalindia850@gmail.com>
2024-10-08 23:26:21 +0530

    Projects

---
+++ React30/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import One from './One'
+import Two from './Two'
+import Three from './Three'
+import Four from './Four'
+import Five from './Five'
+import Six from './Six'
+
+
+const App = () => {
+  return (
+    <div>
+     {/* <One/> */}
+      {/* <Two/> */}
+      {/* <Three/> */}
+      {/* <Four/> */}
+      {/* <Five/> */}
+      <Six/>
+    </div>
+  )
+}
+
+export default App
