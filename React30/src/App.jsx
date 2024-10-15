763ea28cb05286f86448d9ee0bb3f3fadcb46667
yogesh bansal <bansalindia850@gmail.com>
2024-10-15 22:54:30 +0530

    updates

--- React30/src/App.jsx
+++ React30/src/App.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
-import One from './One'
-import Two from './Two'
-import Three from './Three'
-import Four from './Four'
-import Five from './Five'
-import Six from './Six'
+// import React from 'react'
+// import One from './One'
+// import Two from './Two'
+// import Three from './Three'
+// import Four from './Four'
+// import Five from './Five'
+// import Six from './Six'
 
 
+// const App = () => {
+//   return (
+//     <div>
+//      {/* <One/> */}
+//       {/* <Two/> */}
+//       {/* <Three/> */}
+//       {/* <Four/> */}
+//       {/* <Five/> */}
+//       <Six/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import { useEffect, useState } from "react";
+import "./App.css";
 const App = () => {
-  return (
-    <div>
-     {/* <One/> */}
-      {/* <Two/> */}
-      {/* <Three/> */}
-      {/* <Four/> */}
-      {/* <Five/> */}
-      <Six/>
-    </div>
-  )
-}
+const [isOnline, setIsOnline] = useState(true)
+useEffect(() => {
 
-export default App
+  function setOnline() {
+setIsOnline (true)
+  }
+function set0ffline() {
+setIsonline( false)
+}
+window. addEventListener( 'online', setOnline)
+window. addEventListener('offline', set0ffline)
+return (() => {
+window. removeEventListener( 'online', setOnline)
+window. removeEventListener('offline', set0ffline)
+})
+},[])
+return(
+  <>
+  <div>{isOnline ? "connected to internet" : "you are offline"}</div>
+  </>
+)
+}
