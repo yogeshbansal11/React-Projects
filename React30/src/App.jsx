2a4fbdc8105b499f693e9a0c137e76d23159d601
yogesh bansal <bansalindia850@gmail.com>
2024-10-25 22:26:37 +0530

    updates

--- React30/src/App.jsx
+++ React30/src/App.jsx
@@ -1,49 +1,58 @@
-// import React from 'react'
-// import One from './One'
-// import Two from './Two'
-// import Three from './Three'
-// import Four from './Four'
-// import Five from './Five'
-// import Six from './Six'
+import React from 'react'
+import One from './One'
+import Two from './Two'
+import Three from './Three'
+import Four from './Four'
+import Five from './Five'
+import Six from './Six'
+import seven from './seven'
 
 
-// const App = () => {
-//   return (
-//     <div>
-//      {/* <One/> */}
-//       {/* <Two/> */}
-//       {/* <Three/> */}
-//       {/* <Four/> */}
-//       {/* <Five/> */}
-//       <Six/>
-//     </div>
-//   )
-// }
-
-// export default App
-
-import { useEffect, useState } from "react";
-import "./App.css";
 const App = () => {
-const [isOnline, setIsOnline] = useState(true)
-useEffect(() => {
+  return (
+    <div>
+     {/* <One/> */}
+      {/* <Two/> */}
+      {/* <Three/> */}
+      {/* <Four/> */}
+      {/* <Five/> */}
+      {/* <Six/> */}
+      <seven/>
+    </div>
+  )
+}
 
-  function setOnline() {
-setIsOnline (true)
-  }
-function set0ffline() {
-setIsonline( false)
-}
-window. addEventListener( 'online', setOnline)
-window. addEventListener('offline', set0ffline)
-return (() => {
-window. removeEventListener( 'online', setOnline)
-window. removeEventListener('offline', set0ffline)
-})
-},[])
-return(
-  <>
-  <div>{isOnline ? "connected to internet" : "you are offline"}</div>
-  </>
-)
-}
+export default App
+
+
+
+
+
+
+
+
+// import { useEffect, useState } from "react";
+// import "./App.css";
+// const App = () => {
+// const [isOnline, setIsOnline] = useState(true)
+// useEffect(() => {
+
+//   function setOnline() {
+// setIsOnline (true)
+//   }
+// function set0ffline() {
+// setIsonline( false)
+// }
+// window. addEventListener( 'online', setOnline)
+// window. addEventListener('offline', set0ffline)
+// return (() => {
+// window. removeEventListener( 'online', setOnline)
+// window. removeEventListener('offline', set0ffline)
+// })
+// },[])
+// return(
+//   <>
+//   <div>{isOnline ? "connected to internet" : "you are offline"}</div>
+//   </>
+// )
+// }
